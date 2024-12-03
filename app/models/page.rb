dbba2afc97d7d0050f8a223f4a9abf7e65c2f0f2
Anthony Veaudry <anthony@veaudry.pro>
2024-12-03 16:46:04 +0200

    Adds pages methods and validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,23 @@
 class Page < ApplicationRecord
+  has_many :results
+
+  validates :url, presence: true
+  validates :check_type, presence: true
+  validates :selector, presence: true
+  validates :match_text, presence: { if: -> { check_type == 'text' } }
+
+  def run_check!
+    scraper = Scraper.new(url)
+
+    result = case check_type
+    when 'text'
+      scraper.text(selector:).downcase == match_text.downcase
+    when 'presence'
+      scraper.present?(selector:)
+    when 'absence'
+      !scraper.present?(selector:)
+    end
+
+    results.create(success: result)
+  end
 end
